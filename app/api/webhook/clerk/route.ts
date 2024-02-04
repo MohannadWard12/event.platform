75a0f0e5dedc06df7d1938b2a48be1ca3a059217
mohannad <mohannadward824@gmail.com>
2024-02-04 12:08:43 +0200

    edit clerk webhook file

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -52,7 +52,6 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-  console.log("Clerk Event: ", evt);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
