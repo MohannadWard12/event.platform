9c3e5261a24faee7fcba8f853447e642bf10ef42
mohannad <mohannadward824@gmail.com>
2024-02-04 12:04:30 +0200

    edit clerk webhook file path

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -52,6 +52,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
+  console.log("Clerk Event: ", evt);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
@@ -66,6 +67,7 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
+    console.log(newUser);
 
     if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
